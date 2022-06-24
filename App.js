2615f0759a49cbee786922fe0793188b197516c2
Ritika Manish Joshi <ritikajoshi0114@gmail.com>
2022-06-25 00:11:37 +0800

    removed uneccesary imports

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { Provider } from "react-redux"
-// import 'react-native-gesture-handler'
 import { store } from "./src/store/store"
-import Cam from "./src/screens/main-camera"
 import Navigation from "./src/navigation"
 
 
